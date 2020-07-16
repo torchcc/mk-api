bd537b270cb43036b7f48c75e4b29b1633f018b6
troy <1553765526@@qq.com>
2020-07-16 13:31:59 +0800

    wechat notify return_msg string

--- server/controller/pay_controller.go
+++ server/controller/pay_controller.go
@@ -77,7 +77,7 @@ func (c *payController) WechatPayCallback(ctx *gin.Context) {
 	const AckSuccess = "<xml><return_code><![CDATA[SUCCESS]]></return_code></xml>"
 	const S = "<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>"
 	const AckFail = "<xml><return_code><![CDATA[FAIL]]></return_code></xml>"
-	ctx.Writer.Header().Set("Content-Type", "application/xml; charset=utf-8")
+	// ctx.Writer.Header().Set("Content-Type", "application/xml; charset=utf-8")
 	if ok := c.service.WechatPayCallBack(ctx); ok {
 		_, _ = fmt.Fprint(ctx.Writer, S)
 		return
