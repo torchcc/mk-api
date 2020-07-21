2fbcbf72b7401e4911e471b5646c70318df09e4e
troy <1553765526@@qq.com>
2020-07-21 15:44:13 +0800

    debug captcha

--- server/model/captcha_model.go
+++ server/model/captcha_model.go
@@ -3,6 +3,7 @@ package model
 import (
 	"github.com/gomodule/redigo/redis"
 	"mk-api/server/dao"
+	"mk-api/server/util"
 )
 
 type CaptchaModel interface {
@@ -19,6 +20,7 @@ func (db *captchaDatabase) Check(key string, val2Check string) (ok bool) {
 	defer cli.Close()
 	valStored, err := redis.String(cli.Do("GET", key))
 	if err != nil || valStored != val2Check {
+		util.Log.Errorf("检验图形验证码出错, err: [%v], valStored is [%v], val2Check is [%v]", err, valStored, val2Check)
 		return false
 	}
 	return true
@@ -27,7 +29,7 @@ func (db *captchaDatabase) Check(key string, val2Check string) (ok bool) {
 func (db *captchaDatabase) Save(key string, val string) (err error) {
 	cli := db.redisPool.Get()
 	defer cli.Close()
-	_, err = cli.Do("SETEX", key, 180, val)
+	_, err = cli.Do("SETEX", key, 300, val)
 	return
 }
 
