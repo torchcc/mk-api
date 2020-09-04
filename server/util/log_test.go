75bdbcac808b25c4ecb88b0d1dac82b9f5f46353
troy <1553765526@@qq.com>
2020-09-04 19:35:56 +0800

    load cert locally

--- server/util/log_test.go
+++ server/util/log_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestLog(t *testing.T) {
 	Log.WithFields(logrus.
-		Fields{"order_id": 123456, "user_id": 1}).
+		Fields{"order_id": 12345600, "user_id": 1}).
 		Errorf("订单付款失败: err: %s", "服务器错误")
 }
